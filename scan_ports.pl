4dab0549b95e30e0432a86cba992b5511a7f9d59
Konstantin Tokarev <annulen@yandex.ru>
2025-12-03 22:46:44 +0300

    Refactor, parse nmap output

--- scan_ports.pl
+++ scan_ports.pl
@@ -3,6 +3,7 @@
 use URI::Split qw(uri_split);
 use strict;
 use warnings;
+use autodie;
 
 my %hosts_by_port;
 
@@ -13,23 +14,32 @@ sub dump_hosts {
     }
 }
 
-sub do_scan {
+sub do_nmap_scan {
     my $port = shift;
     my @hosts = @_;
 
-    my $cmd = "nmap -n -Pn -oG - -p $port @hosts | grep -wF Ports:";
-    system($cmd) == 0 or die "nmap failed: $!";
+    open my $nmap, '-|', "nmap -n -Pn -oG - -p $port @hosts | grep -wF Ports:";
+    while(<$nmap>) {
+        if (my ($host, $port, $info) = m{^Host: ([.0-9]+).*Ports: (\d+)/(.*)}) {
+            print "socks5://$host:$port\t$info\n";
+        } else {
+            warn "Could not parse nmap output: $_\n";
+        }
+    }
 }
 
 while(<>) {
     chomp;
     my ($scheme, $auth) = uri_split($_);
-    next if $scheme !~ /^socks/;
-    my ($host, $port) = $auth =~ /^(.*):(.*)$/;
-    push $hosts_by_port{$port}->@*, $host;
+    next if $scheme ne 'socks5';
+    if (my ($host, $port) = $auth =~ /^(.*):(.*)$/) {
+        push $hosts_by_port{$port}->@*, $host;
+    } else {
+        warn "Could not parse host:port pair from URI: $_\n";
+    }
 }
 
 while (my ($port, $hosts) = each %hosts_by_port) {
-    do_scan $port, @$hosts;
+    do_nmap_scan $port, @$hosts;
 }
 
